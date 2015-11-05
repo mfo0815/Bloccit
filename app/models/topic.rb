0e1d239511d2fc5ed690b5f2301950fe952cfe64
Mike <mfo0815@gmail.com>
2015-11-05 16:18:38 -0500

    checkpoint 35 assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-  has_many :sponsored_posts, dependent: :destroy
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 15 }, presence: true
+  has_many :sponsored_posts, dependent: :destroy
 end
