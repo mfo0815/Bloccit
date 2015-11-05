7c4ac974597eec0484fe207515ced14c822e77b4
Mike <mfo0815@gmail.com>
2015-11-05 16:08:15 -0500

    addl input

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe Topic, type: :model do
 
   it { should have_many(:posts) }
 
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(15) }
+
   describe "attributes" do
     it "should respond to name" do
       expect(topic).to respond_to(:name)
