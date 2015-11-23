c1132981fc80c535a534e952356be2b922f6b3e3
Mike <mfo0815@gmail.com>
2015-11-23 17:24:13 -0500

    checkpoint-47-assignment

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -9,11 +9,10 @@ require 'rails_helper'
    let(:user_post) { create(:post, topic: my_topic, user: other_user) }
    let(:my_vote) { Vote.create!(value: 1) }
 
- # #17
    context "guest" do
      describe "POST up_vote" do
        it "redirects the user to the sign in view" do
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          expect(response).to redirect_to(new_session_path)
        end
      end
@@ -26,7 +25,6 @@ require 'rails_helper'
     end
    end
 
- # #18
    context "signed in user" do
      before do
        create_session(my_user)
@@ -34,71 +32,67 @@ require 'rails_helper'
      end
 
      describe "POST up_vote" do
- # #19
        it "the users first vote increases number of post votes by one" do
          votes = user_post.votes.count
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          expect(user_post.votes.count).to eq(votes + 1)
        end
 
- # #20
        it "the users second vote does not increase the number of votes" do
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          votes = user_post.votes.count
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          expect(user_post.votes.count).to eq(votes)
        end
 
- # #21
        it "increases the sum of post votes by one" do
          points = user_post.points
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          expect(user_post.points).to eq(points + 1)
        end
 
- # #22
        it ":back redirects to posts show page" do
          request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          expect(response).to redirect_to([my_topic, user_post])
        end
 
- # #23
        it ":back redirects to posts topic show" do
          request.env["HTTP_REFERER"] = topic_path(my_topic)
-         post :up_vote, post_id: user_post.id
+         post :up_vote, format: :js, post_id: my_post.id
          expect(response).to redirect_to(my_topic)
        end
      end
+
      describe "POST down_vote" do
        it "the users first vote increases number of post votes by one" do
          votes = user_post.votes.count
-         post :down_vote, post_id: user_post.id
+         post :down_vote, format: :js, post_id: my_post.id
          expect(user_post.votes.count).to eq(votes + 1)
        end
 
        it "the users second vote does not increase the number of votes" do
-         post :down_vote, post_id: user_post.id
+         post :down_vote, format: :js, post_id: my_post.id
          votes = user_post.votes.count
-         post :down_vote, post_id: user_post.id
+         post :down_vote, format: :js, post_id: my_post.id
          expect(user_post.votes.count).to eq(votes)
        end
 
        it "decreases the sum of post votes by one" do
          points = user_post.points
-         post :down_vote, post_id: user_post.id
+         post :down_vote, format: :js, post_id: my_post.id
          expect(user_post.points).to eq(points - 1)
        end
 
        it ":back redirects to posts show page" do
          request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
-         post :down_vote, post_id: user_post.id
+         post :down_vote, format: :js, post_id: my_post.id
          expect(response).to redirect_to([my_topic, user_post])
        end
 
        it ":back redirects to posts topic show" do
          request.env["HTTP_REFERER"] = topic_path(my_topic)
-         post :down_vote, post_id: user_post.id
+         post :down_vote, format: :js, post_id: my_post.id
          expect(response).to redirect_to(my_topic)
        end
      end
